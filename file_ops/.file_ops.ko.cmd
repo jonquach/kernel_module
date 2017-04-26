f310d8d7ec100cdd60fbafab054bddb15e7d00e7
docteur <emilien.lancelot@epitech.eu>
2017-04-26 12:24:46 -0700

    all stuff

---
+++ file_ops/.file_ops.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/etu1/Documents/TP4/1/file_ops/file_ops.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/etu1/Documents/TP4/1/file_ops/file_ops.ko /home/etu1/Documents/TP4/1/file_ops/file_ops.o /home/etu1/Documents/TP4/1/file_ops/file_ops.mod.o
---
+++ file_ops/.tmp_versions/file_ops.mod
@@ -0,0 +1,2 @@
+/home/etu1/Documents/TP4/1/file_ops/file_ops.ko
+/home/etu1/Documents/TP4/1/file_ops/file_ops.o
